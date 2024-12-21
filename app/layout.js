af39091e478f4a24bfeaea394cee1a52d1647e5e
aircrushin <1216564567@qq.com>
2024-12-21 23:54:59 +0800

    add signin page

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,49 @@
-'use client'
+"use client";
 
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Navbar from '@/app/components/layout/Navbar';
-import { usePathname } from 'next/navigation';
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import { Toaster } from 'sonner';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/app/components/layout/Navbar";
+import { usePathname } from "next/navigation";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-  const isHomePage = pathname === '/';
-
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>
-          {!isHomePage && <Navbar />}
+          <NavbarWrapper />
           {children}
           <Toaster position="top-center" />
         </body>
       </html>
     </ClerkProvider>
   );
+}
+
+function NavbarWrapper() {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
+  const isSignInPage = pathname === "/sign-in";
+  const isSignUpPage = pathname === "/sign-up";
+
+  if (isHomePage || isSignInPage || isSignUpPage) {
+    return null;
+  }
+
+  return <Navbar />;
 }
